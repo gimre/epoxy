1657d41da2cb3f8225d0ca35952971e75cb851ee
Gabriel Imre <gimre@endava.net>
2017-06-14 11:36:53 +0300

    added testing

--- test/index.js
+++ test/index.js
@@ -1,5 +1,31 @@
 
+const should = require( 'chai' ).expect( )
 const Container = require( '../index' )
-const ioc = new Container( './fixture' )
 
-console.log( ioc.create( './app' ) )
+process.env.DEBUG = '='
+
+describe( 'Container constructor', ( ) => {
+    it( 'should accept no parameters', ( ) => {
+        new Container
+    } )
+    it( 'should accept an optional provider path', ( ) => {
+        new Container( './fixture' )
+    } )
+    it( 'should accept an optional list of provider paths', ( ) => {
+        new Container( [ './fixture', './fixture2' ] )
+    } )
+} )
+
+// describe( 'Container operation', ( ) => {
+//     beforeEach( ( ) => {
+//         this.container = new Container( './fixture' )
+//     } )
+
+//     it( 'should resolve module with no dependencies', ( ) => {
+//         this.container.create( 'moduleWithNoDependencies' )
+//     } )
+
+//     afterEach( ( ) => {
+//         this.container = null
+//     } )
+// } )
